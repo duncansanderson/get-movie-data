d4e5848d3476bf791dc711d234cb7e029caa9177
Duncan Sanderson <duncan@duncansanderson.co.uk>
2021-07-21 18:37:58 +0100

    FORMAT (JavaScript) Tidy up code

--- index.js
+++ index.js
@@ -2,14 +2,8 @@ const { getBasicMovies, saveMovies, getFullMovieData } = require('./modules/util
 
 const getMovieData = async () => {
     const movieData = await getBasicMovies()
-    // const completeMovieData = await getFullMovieData(movieData)
     await getFullMovieData(movieData)
         .then((result) => saveMovies(result))
-    // console.log(completeMovieData)
-    // saveMovies(completeMovieData)
-    // Get detailed data from OMDB
-    // saveMovies(movies)
 }
 
 getMovieData()
-
--- modules/utils.js
+++ modules/utils.js
@@ -11,7 +11,7 @@ const basicMoviesLocation = path.join(__dirname, '../assets/basicMovies.json')
 const completeMoviesLocation = path.join(__dirname, '../assets/movies.json')
 
 /**
- * Should read the movies @basicMoviesLocation
+ * Read the movies @basicMoviesLocation
  * path and convert to js object
  */
 const getBasicMovies = async () => {
@@ -23,6 +23,11 @@ const getBasicMovies = async () => {
     }
 }
 
+/**
+ * write the movie JSON data to the
+ * file @completeMoviesLocation path
+ * @param {*} movies - convert to JSON and write to file
+ */
 const saveMovies = async (movies) => {
     try {
         await fs.writeFile(completeMoviesLocation, JSON.stringify(movies, null, 2))
@@ -31,20 +36,16 @@ const saveMovies = async (movies) => {
     }
 }
 
-// const getFullMovieData = async (movies) => {
-//     const fullData = []
-//     movies.forEach(async (movie) => {
-//         const movieData = await getMovie(movie)
-//         fullData.push(movieData)
-//     })
-
-//     return Promise.all(fullData)
-// }
-
+/**
+ * Query the oMDB API to get more
+ * detailed information about each movie
+ * @param {*} movies - array of movie objects
+ * @returns detailed movie date
+ */
 const getFullMovieData = async (movies) => {
     const fullData = []
 
-    await Promise.all(movies.map(async (movie, index) => {
+    await Promise.all(movies.map(async (movie) => {
 
         const title = (movie.altTitle) ? movie.altTitle : movie.title
         const url = new URL(process.env.APIURL)
@@ -64,33 +65,12 @@ const getFullMovieData = async (movies) => {
 
     return fullData
 }
-// async function getMovieData() {
-//     await Promise.all(movieJson.map(async (movie) => {
-//         const fetchUrl = `${url}?apikey=${apiKey}&t=${movie.title.toLowerCase()}&year=${movie.year}&plot=full`
 
-//         fetch(fetchUrl)
-//             .then((resp) => resp.json())
-//             .then((data) => finalData.push(data))
-//     }))
-
-//     return finalData
-// }
-
-// const getMovie = async (movie) => {
-//     const fetchUrl = new URL(process.env.APIURL)
-//     fetchUrl.searchParams.set('apikey', process.env.APIKEY)
-//     fetchUrl.searchParams.set('t', movie.title)
-//     fetchUrl.searchParams.set('year', movie.year)
-//     fetchUrl.searchParams.set('plot', 'full')
-
-//     fetch(fetchUrl)
-//         .then((resp) => resp.json())
-//         .then((data) => objectKeysToLowerCase(data))
-//         .then((data) => removeUnwantedData(data))
-//         // .then((data) => fullData.push(data))
-//         .catch((error) => console.log(error))
-// }
-
+/**
+ * Make all object keys lowercase.
+ * @param {*} data
+ * @returns data containing objects with lowercase keys.
+ */
 const objectKeysToLowerCase = (data) => {
     const cleanData = {}
 
@@ -101,24 +81,17 @@ const objectKeysToLowerCase = (data) => {
     return cleanData
 }
 
+/**
+ * Remove unused object data
+ * @param {*} data
+ * @returns cleaned objects
+ */
 const removeUnwantedData = (data) => {
     return Object.keys(dataModel).reduce((obj, key) => {
         obj[key] = data[key]
         return obj
     }, {})
 }
-
-// const removeUnwantedData = (data) => {
-//     // console.log('data model', dataModel)
-//     // const cleanData = data
-//     // console.log('c',data)
-//     const cleanData = Object.keys(dataModel).reduce((obj, key) => {
-//         obj[key] = data[key]
-//         return obj
-//     }, {})
-//     // console.log('d', cleanData)
-//     return cleanData
-// }
 
 module.exports = {
     getBasicMovies,
